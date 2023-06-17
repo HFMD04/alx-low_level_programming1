b22b29215dee7ccaaaf58f4119d18829de275e65
hafsa miad <hfsamiad2021@gmail.com>
2023-06-17 16:35:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints "Programming is like building a multilingu
+ *  puzzle, followed by a newline."
+ *  return 0.
+ */
+int main(void)
+
+{
+	puts("\"Progrmming is like building a multilingaul puzzle");
+
+	return(0);
+}	
