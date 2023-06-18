dac54c88666e786d62f7d2bc6262549f04dc0c60
hafsa miad <hfsamiad2021@gmail.com>
2023-06-18 16:52:10 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - A program that print a line using the printf f
- unction 
- * Return: 0 (Success)
- */
- 
-int main(void) 
-
+* main - Entry point
+*
+* Description: A C program that prints with printf function
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a 
-piece of art,\n");
-	return (0);
-}	
-
-	
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
