2bef888a96811d4e3f5a15fd1ea5af8df8b3946d
hafsa miad <hfsamiad2021@gmail.com>
2023-06-18 15:45:24 -0700

    betty 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,14 @@
 
 
 /**
- * main - A program that prints a line with puts functio
- n 
- * Return: 0 (Success) 
+ * main - A program that prints a line with puts function
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 
 {
 	puts("\"Progrmming is like building a multilingaul puzzle");
 
-	return(0);
-}	
+	return (0);
+}
